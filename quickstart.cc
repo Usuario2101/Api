b92f5ae5d6f0b55b8cb137c0a9dbd3d5b6ef4750
Usuario2101 <126848119+Usuario2101@users.noreply.github.com>
2023-10-29 06:00:16 -0600

    Add files via upload

---
+++ quickstart.cc
@@ -0,0 +1,40 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! [all]
+#include "google/cloud/secretmanager/v1/secret_manager_client.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
+    return 1;
+  }
+
+  namespace secretmanager = ::google::cloud::secretmanager_v1;
+  auto client = secretmanager::SecretManagerServiceClient(
+      secretmanager::MakeSecretManagerServiceConnection());
+
+  auto const parent = std::string("projects/") + argv[1];
+  for (auto secret : client.ListSecrets(parent)) {
+    if (!secret) throw std::move(secret).status();
+    std::cout << secret->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (google::cloud::Status const& status) {
+  std::cerr << "google::cloud::Status thrown: " << status << "\n";
+  return 1;
+}
+//! [all]
